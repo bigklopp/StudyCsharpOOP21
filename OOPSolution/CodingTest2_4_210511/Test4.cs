175c04a4a14cf1fd726be61bd590f5f0ddaba7f0
bigklopp <zmxncbv4@naver.com>
2021-05-21 09:32:51 +0900

    수업 완전 이후 수정

--- OOPSolution/CodingTest2_4_210511/Test4.cs
+++ OOPSolution/CodingTest2_4_210511/Test4.cs
@@ -8,21 +8,30 @@ namespace CodingTest2_4_210511
         static void Main(string[] args)
         {
             
-            Hashtable dic = new Hashtable();
+            Dictionary<string,string> dic = new Dictionary();
 
-            dic["Red"] = "빨간색";
-            dic["Orange"] = "주황색";
-            dic["Yellow"] = "노란색";
-            dic["Green"] = "초록색";
-            dic["Blue"] = "파란색";
-            dic["Navy"] = "남색";
-            dic["Purple"] = "보라색";
+            dic.Add("Red", "빨간색");
+            dic.Add("Orange", "주황색");
+            dic.Add("Yellow","노란색");
+            dic.Add("Green", "초록색");
+            dic.Add("Blue", "파란색");
+            dic.Add("Navy", "남색");
+            dic.Add("Purple", "보라색");
 
             Console.WriteLine($"무지개 색은 {dic["Red"]}, {dic["Orange"]}, {dic["Yellow"]}, {dic["Green"]}, {dic["Blue"]}, {dic["Navy"]}, {dic["Purple"]}, 입니다.\n");
-            
+
+            Console.Write($"무지개 색은 ");
+            foreach (var item in dic)
+            {
+                Console.Write($"{item.value}");
+            }
+
+            Console.WriteLine($"입니다.");
+
             Console.WriteLine("Key와 Value 확인");
             
-            Console.WriteLine($"Purple은 {dic["Purple"]}입니다.");
+
+            //Console.WriteLine($"Purple은 {dic["Purple"]}입니다.");
 
         }
     }
